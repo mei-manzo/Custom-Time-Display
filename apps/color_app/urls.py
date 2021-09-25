03e5771cfafe585fdde5bc47a2054ca6e3be43ed
Mei Manzo <mmanzo806@gmail.com>
2021-09-25 13:18:56 -0700

    adding basic toolkit

--- apps/color_app/urls.py
+++ apps/color_app/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, path
 from . import views           
 urlpatterns = [
-    url(r'^$', views.index)     
+    path('', views.index),    
 ]
