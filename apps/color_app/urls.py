b7e0445cdd5a22b95d8b1befd8d2fe8ba5496300
Mei Manzo <mmanzo806@gmail.com>
2021-09-25 17:44:54 -0700

    added basic website code

--- apps/color_app/urls.py
+++ apps/color_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path('', views.api), #need to leave localhost for login/registration app
     path('home', views.index),   
     path('profile', views.profile),    
+    path('check_name', views.check_name),
 ]
--- apps/color_app/views.py
+++ apps/color_app/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import *
 from datetime import date 
+import datetime
 
 def index(request):
-    today = date.today()
+    today = datetime.datetime.now()
+    color = "yellow"
+    recent_name = Name.objects.last() #grabs most recent name  
     context = {
         "today": today,
+        "color": color,
+        "recent": recent_name,
     }
     return render(request, "index.html", context)
 
 def profile(request):
-    return render(request, "profile.html")
+    number_of_names = Name.objects.count()
+    context = {
+        "number":number_of_names,
+    }
+    return render(request, "profile.html", context)
+
+def check_name(request):
+    if 'name' in request.POST:
+        name = request.POST['name']
+        new_name = Name.objects.create(name = name)
+    else:
+        name = False
+    return redirect('/home')
 
