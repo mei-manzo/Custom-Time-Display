cf19a892aef7db0b9ece454bc4332d31bdba2fcf
Mei Manzo <mmanzo806@gmail.com>
2021-09-25 18:00:56 -0700

    debugging color randomizer

--- apps/color_app/views.py
+++ apps/color_app/views.py
@@ -2,15 +2,20 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import *
 from datetime import date 
 import datetime
+import random
+
+colors = ["red", "yellow", "blue", "green", "purple"]
 
 def index(request):
     today = datetime.datetime.now()
     color = "yellow"
     recent_name = Name.objects.last() #grabs most recent name  
+    rand_color = random.randint(0, len(colors)-1)
     context = {
         "today": today,
         "color": color,
         "recent": recent_name,
+        "color": rand_color,
     }
     return render(request, "index.html", context)
 
