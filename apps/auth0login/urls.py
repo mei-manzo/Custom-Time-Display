172e084ca8ccf462eea51a85c9c9997011265074
Mei Manzo <mmanzo806@gmail.com>
2021-09-25 20:41:46 -0700

    added auth0 urls.py file

---
+++ apps/auth0login/urls.py
@@ -0,0 +1,9 @@
+# auth0login\urls.py
+
+urlpatterns = [
+    path('', views.index),
+    path('dashboard', views.dashboard),
+    path('logout', views.logout),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('social_django.urls')),
+]
